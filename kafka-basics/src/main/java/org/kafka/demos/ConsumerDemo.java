f135ea6a9d9b7a37a47a0c3ecbd6621d165838a7
cononi <ekgoqhffo@gmail.com>
2023-08-31 15:42:47 +0900

    [feat] Consumer 기능 추가
    - Producer로부터 요청 받은 메세지를 받아와서 소비하기.

---
+++ kafka-basics/src/main/java/org/kafka/demos/ConsumerDemo.java
@@ -0,0 +1,61 @@
+package org.kafka.demos;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.ObjectSerializationCache;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        log.info("I am a Kafka Consumer!");
+
+        // create Producer Properties - 프로듀서 설정 생성
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092"); // 브로커와 연결
+
+
+        // create consumer configs - 역직렬화
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create a consumer - 컨슈머 생성
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Subscribe to a Topic - 토픽 구독
+//        consumer.subscribe(Arrays.asList("topic1","topic2"));
+        consumer.subscribe(Arrays.asList(topic));
+
+        // poll for data - 토픽으로 부터 데이터 받아오기
+        while (true) {
+            log.info("Polling");
+
+            // 1000 밀리초동안 대기
+            ConsumerRecords<String, String> recodes =
+                    consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String,String> record : recodes){
+                log.info("key: " + record.key() + ", value: " + record.value());
+                log.info("partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
+}
