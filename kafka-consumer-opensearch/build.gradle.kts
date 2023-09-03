a4a7eb9a27ba146e4fc7defe3e1e4187f840c45a
cononi <ekgoqhffo@gmail.com>
2023-09-03 21:36:18 +0900

    [feat] opensearch 생성

--- kafka-consumer-opensearch/build.gradle.kts
+++ kafka-consumer-opensearch/build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://search.maven.org/artifact/org.opensearch.client/opensearch-rest-high-level-client/1.2.4/jar
+    implementation("org.opensearch.client:opensearch-rest-high-level-client:2.9.0")
+
+    // https://search.maven.org/artifact/com.google.code.gson/gson/2.9.0/jar
+    implementation("com.google.code.gson:gson:2.10.1")
 }
 
 tasks.test {
---
+++ kafka-consumer-opensearch/src/main/java/org/kafka/demos/OpenSearchConsumer.java
@@ -0,0 +1,7 @@
+package org.kafka.demos;
+
+public class OpenSearchConsumer {
+    public static void main(String[] args) {
+
+    }
+}
