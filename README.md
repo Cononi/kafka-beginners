20799f011216f1a630c65e0675691cf5f4bf3227
cononi <ekgoqhffo@gmail.com>
2023-09-04 10:09:46 +0900

    [fix] cache 초기화

--- README.md
+++
@@ -1,2 +0,0 @@
-# kafka-beginners
-카프카 공부를 위한 프로젝트 -  Java
--- settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-rootProject.name = "kafka-beginners"
-include("kafka-basics")
-include("kafka-producer-wikimedia")
-include("common")
-include("kafka-consumer-opensearch")
